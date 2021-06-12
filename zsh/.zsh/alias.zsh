d773d0ebab42f764a293e2985e089ea81781eb2f
amazingefren <efrentheroker@hotmail.com>
2021-06-11 20:08:16 -0600

    reflector

--- zsh/.zsh/alias.zsh
+++ zsh/.zsh/alias.zsh
@@ -20,6 +20,7 @@ alias fuck=b
 alias temps='watch -n.5 "grep \"^[c]pu MHz\" /proc/cpuinfo; echo && sensors k10temp-pci-00c3 amdgpu-pci-2d00"'
 alias fm='vifm'
 # PS => PROCS
+alias ua='sudo reflector --verbose --country US --latest 5 --protocol https --sort rate --save /etc/pacman.d/mirrorlist'
 
 
 # n ()
