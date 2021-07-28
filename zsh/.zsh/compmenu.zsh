0273dc1f154814cda8e0b0a42b78ba1220b50a2a
amazingefren <efrentheroker@hotmail.com>
2021-07-28 01:54:55 -0600

    zsh better completion

---
+++ zsh/.zsh/compmenu.zsh
@@ -0,0 +1,31 @@
+zmodload -i zsh/complist
+
+export LS_COLORS="$(vivid generate $HOME/.zsh/assets/vividtheme.yml)"
+
+zstyle ':completion:*' completer _extensions _expand _complete _ignored _approximate
+zstyle ':completion::complete:*' use-cache on
+zstyle ':completion::complete:*' cache-path ~/.zsh_cache/$HOST
+zstyle ':completion:*' menu select=1
+zstyle ':completion:*:descriptions' format '%F{cyan}%B ----- %d -----%f'
+zstyle ':completion:*' group-name ''
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*' complete-options true # DIRECTORY STACK LETS GO
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|=*' 'l:|=* r:|=*'
+### MOOD: sad
+# SEE: https://www.zsh.org/mla/users/2020/msg00229.html
+# zstyle ':completion:*' file-list true all
+###
+
+setopt nocaseglob
+setopt auto_menu
+setopt globdots
+# setopt menu_complete
+# setopt list_packed
+
+bindkey -M menuselect 'H' vi-backward-char
+bindkey -M menuselect 'K' vi-up-line-or-history
+bindkey -M menuselect 'J' vi-down-line-or-history
+bindkey -M menuselect 'L' vi-forward-char
+bindkey -M menuselect ' ' accept-and-infer-next-history
+bindkey -M menuselect '/' history-incremental-search-forward
+bindkey -M menuselect '^[[Z' reverse-menu-complete
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,32 +1,22 @@
 source ~/.znap/zsh-snap/znap.zsh
 autoload -Uz compinit 
 compinit
+
 source "$HOME/.zsh/plugins.zsh"
 source "$HOME/.zsh/history.zsh"
 source "$HOME/.zsh/alias.zsh"
 source "$HOME/.zsh/prompt.zsh"
-zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|=*' 'l:|=* r:|=*'
-setopt menu_complete
-setopt nocaseglob
-
-# . "/usr/share/LS_COLORS/dircolors.sh"
-export LS_COLORS="$(vivid generate $HOME/.zsh/assets/vividtheme.yml)"
+source "$HOME/.zsh/compmenu.zsh"
 
 export EDITOR=/usr/bin/nvim
 export VISUAL=/usr/bin/nvim
-
+export PATH=$PATH:$HOME/go/bin
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
-q () {
-  exit
-}
 
 if [ -f ~/.last_pwd ]; then
     cd `cat ~/.last_pwd`
 fi 
-
-export PATH=$PATH:$HOME/go/bin
 
 if [ -z "${DISPLAY}" ] && [ "$(tty)" = "/dev/tty1" ]; then
     exec startx
