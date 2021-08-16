39c89799009d48b043e881aee10e75456791a587
amazingefren <efrentheroker@hotmail.com>
2021-08-16 17:28:07 -0600

    laptop font size, alacritty default dwm, sctachpad no work

--- dwm/.dwm/config.h
+++ dwm/.dwm/config.h
@@ -63,9 +63,9 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_base, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "kitty", NULL };
-static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "kitty", "-T", scratchpadname, "", "", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
+static const char scratchpadname[] = " scratchpad";
+static const char *scratchpadcmd[] = { "alacritty", "-t", scratchpadname, "", "", NULL };
 
 #include "selfrestart.c"
 
