eb9734161a92fbb17f83a40f25ba05e5f06cd9b8
amazingefren <efrentheroker@hotmail.com>
2021-08-16 17:31:20 -0600

    gonna switch dwm scratchpad patch

--- dwm/.dwm/config.h
+++ dwm/.dwm/config.h
@@ -64,8 +64,8 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_base, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
-static const char scratchpadname[] = " scratchpad";
-static const char *scratchpadcmd[] = { "alacritty", "-t", scratchpadname, "", "", NULL };
+static const char scratchpadname[] = "scratchpad";
+static const char *scratchpadcmd[] = { "alacritty -t scratchpad", "", "", "", "", NULL };
 
 #include "selfrestart.c"
 
