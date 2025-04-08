95745b14e3ae7c2b500d7b3b5ddf94e49dc97cdd
Efren Castro <git@amazingefren.com>
2025-04-07 21:39:20 -0600

    just some random codecompanion shit

--- nvim/.config/nvim/lua/plugins/codecompanion.lua
+++ nvim/.config/nvim/lua/plugins/codecompanion.lua
@@ -1,18 +1,34 @@
+-- local default_model = "google/gemini-2.5-pro-preview-03-25"
+-- local default_model = "google/gemini-2.5-pro-preview-03-25"
+
+-- local available_models = {
+--   "google/gemini-2.0-flash-001",
+--   "google/gemini-2.5-pro-preview-03-25",
+--   "anthropic/claude-3.7-sonnet",
+--   "anthropic/claude-3.5-sonnet",
+--   "openai/gpt-4o-mini",
+-- }
+local default_model = "openrouter/quasar-alpha"
+-- local default_inline_model = "google/gemini-2.0-flash-001"
+
 return {
   "olimorris/codecompanion.nvim",
   lazy = true,
+  -- branch = "feat/move-to-function-calling",
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
     "j-hui/fidget.nvim",
   },
 
-  keys = {
-    { "<leader>aa" , "<CMD>CodeCompanionActions<CR>", mode = {"n", "v"}, desc = "Code Companion Actions" },
-    { "<leader>ae" , ":CodeCompanion<CR>", mode = {"v"}, desc = "Code Companion Inline Edit" },
-    { "<C-b>" , "<CMD>CodeCompanionChat Toggle<CR>", mode = {"n", "v"}, desc = "Code Companion Chat" },
-    { "<C-l>" , "<CMD>CodeCompanionChat Add<CR><Esc>", mode = {"v"}, desc = "Add to Code Companion Chat"}
-  },
+  keys = function ()
+    return {
+      { "<leader>aa" , "<CMD>CodeCompanionActions<CR>", mode = {"n", "v"}, desc = "Code Companion Actions" },
+      { "<leader>ae" , ":CodeCompanion<CR>", mode = {"v"}, desc = "Code Companion Inline Edit" },
+      { "<C-b>" , "<CMD>CodeCompanionChat Toggle<CR>", mode = {"n", "v"}, desc = "Code Companion Chat" },
+      { "<C-l>" , "<CMD>CodeCompanionChat Add<CR><Esc>", mode = {"v"}, desc = "Add to Code Companion Chat"}
+    }
+  end,
 
   init = function ()
     require("plugins.codecompanion.fidget-spinner"):init()
@@ -23,29 +39,48 @@ return {
     return {
       strategies = {
         chat = {
-          adapter = "copilot"
+          -- adapter = "gemini"
+          adapter = "openrouter"
+          -- adapter = "copilot"
         },
 
         inline = {
-          adapter = "copilot",
+          -- adapter = "gemini",
+          adapter = "openrouter_inline",
+          -- adapter = "copilot",
         },
       },
-      display = {
-        diff = {
-          -- layout = "horizontal",
-          provider = "mini_diff",
-          opts = {
-            "internal",
-            "filler",
-            "closeoff",
-            "algorithm:histogram", -- https://adamj.eu/tech/2024/01/18/git-improve-diff-histogram/
-            "indent-heuristic",  -- https://blog.k-nut.eu/better-git-diffs
-            "followwrap",
-            "linematch:120",
-          },
-        }
-      },
       adapters = {
+        openrouter = function()
+          load_secret_env({ env = "OPENROUTER_API_KEY", file_name = "openrouter_api_key"})
+          return require("codecompanion.adapters").extend("openai_compatible", {
+            env = {
+              url = "https://openrouter.ai/api",
+              api_key = "OPENROUTER_API_KEY",
+              chat_url = "/v1/chat/completions",
+            },
+            schema = {
+              model = {
+                default = default_model,
+              },
+            },
+          })
+        end,
+        openrouter_inline = function()
+          load_secret_env({ env = "OPENROUTER_API_KEY", file_name = "openrouter_api_key"})
+          return require("codecompanion.adapters").extend("openai_compatible", {
+            env = {
+              url = "https://openrouter.ai/api",
+              api_key = "OPENROUTER_API_KEY",
+              chat_url = "/v1/chat/completions",
+            },
+            schema = {
+              model = {
+                default = default_model,
+              },
+            },
+          })
+        end,
         copilot = function ()
           return require("codecompanion.adapters").extend("copilot", {
             schema = {
@@ -54,8 +89,44 @@ return {
               }
             }
           })
+        end,
+        gemini = function ()
+          load_secret_env({ env = "GEMINI_API_KEY", file_name = "gemini_api_key"})
+          return require("codecompanion.adapters").extend("gemini", {
+            env = {
+              api_key = "GEMINI_API_KEY"
+            },
+            schema = {
+              model = {
+                default = "gemini-2.5-pro-exp-03-25"
+              }
+            }
+          })
         end
-      }
+      },
+      display = {
+        chat = {
+          window = {
+            layout = "float",
+            width = 0.8,
+          }
+        },
+        diff = {
+          -- layout = "horizontal",
+          provider = "mini_diff",
+          -- opts = {
+          --   "internal",
+          --   "filler",
+          --   "closeoff",
+          --   "algorithm:histogram", -- https://adamj.eu/tech/2024/01/18/git-improve-diff-histogram/
+          --   "indent-heuristic",  -- https://blog.k-nut.eu/better-git-diffs
+          --   "followwrap",
+          --   "linematch:120",
+          -- },
+        }
+      },
+      -- adapters = {
+      -- }
     }
   end
 }
